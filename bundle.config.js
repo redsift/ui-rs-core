3b3a22f10ac79e440284abdb3612f9e4f8e242fe
Martin Hecher <martin@redsift.io>
2016-06-13 17:57:03 +0100

    fix: use correct style/js index files

--- bundle.config.js
+++ bundle.config.js
@@ -13,11 +13,11 @@ var defaultConfig = {
 var bundleConfig = {
   mainJS: {
     name: 'ui-rs-core',
-    indexFile: '../src/index.js' // FIXXME: has to be relative to './gulp-tasks'
+    indexFile: './src/index.js'
   },
   styles: [{
     name: 'ui-rs-core',
-    indexFile: '../src/index.import.styl'  // FIXXME: has to be relative to './gulp-tasks'
+    indexFile: './src/index.import.styl'
   }]
 };
 
